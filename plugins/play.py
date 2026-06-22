28667cb50df3d1348dc63fdee83b66cf3545a120
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:41:52 +0200

    Update play.py

--- plugins/play.py
+++ plugins/play.py
@@ -1,1 +1,9 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.text & filters.regex(r"^شغل (.+)"))
+async def play_command(_, message):
+    song = message.text[4:].strip()
+
+    await message.reply_text(
+        f"🎵 جاري البحث عن:\n\n{song}"
+    )
