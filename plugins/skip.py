e85b278a581efefee3d37e9bae672bd5a4181848
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:42:34 +0200

    Add skip command for song in skip.py

--- plugins/skip.py
+++ plugins/skip.py
@@ -1,1 +1,7 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.text & filters.regex("^تخطي$"))
+async def skip_command(_, message):
+    await message.reply_text(
+        "⏭ تم تخطي الأغنية"
+    )
