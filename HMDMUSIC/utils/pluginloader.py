9ecf90604233bb314be8f7f89f31ac67b0fa2022
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:39:57 +0200

    Implement plugin loading functionality
    Add function to load plugins from the plugins directory

--- HMDMUSIC/utils/pluginloader.py
+++ HMDMUSIC/utils/pluginloader.py
@@ -1,1 +1,13 @@
+from pathlib import Path
+import importlib
 
+def load_plugins():
+    plugins = Path("HMDMUSIC/plugins")
+
+    for plugin in plugins.glob("*.py"):
+        if plugin.stem.startswith("_"):
+            continue
+
+        importlib.import_module(
+            f"HMDMUSIC.plugins.{plugin.stem}"
+        )
