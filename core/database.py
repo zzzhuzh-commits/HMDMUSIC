542de159ccc6a7a5d14c9bf7c56ea503a4a2eeca
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:43:52 +0200

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,1 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL
 
+mongo = AsyncIOMotorClient(MONGO_URL)
+
+db = mongo["HMDMUSIC"]
+
+settingsdb = db.settings
+queuedb = db.queue
+usersdb = db.users
