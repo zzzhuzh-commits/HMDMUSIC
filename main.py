bd5a3fe7b537984eed4b253b827b1200ced80d03
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:40:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
+from HMDMUSIC.utils.pluginloader import load_plugins
+
 app = Client(
     "HMDMUSIC",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
+    bot_token=BOT_TOKEN
 )
 
-import HMDMUSIC.plugins.start
-import HMDMUSIC.plugins.help
+load_plugins()
 
 app.run()
