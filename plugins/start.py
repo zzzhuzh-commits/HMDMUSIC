6b6c6fe74f80e6d5d79f05180555f6bbd76e6b3f
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:36:29 +0200

    Add start message handler for greeting

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,8 @@
+from pyrogram import filters
+from pyrogram import Client
 
+@Client.on_message(filters.text & filters.regex("^هلا$"))
+async def start_message(_, message):
+    await message.reply_text(
+        "🎵 أهلاً بك في HMDMUSIC"
+    )
