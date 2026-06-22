4ff3c542e239a8d6cb18c844fb5e473d292a20b6
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:46:45 +0200

    Implement queue functions for chat management

--- core/queue.py
+++ core/queue.py
@@ -1,1 +1,20 @@
+QUEUE = {}
 
+def add_to_queue(chat_id, track):
+    if chat_id not in QUEUE:
+        QUEUE[chat_id] = []
+
+    QUEUE[chat_id].append(track)
+
+def get_queue(chat_id):
+    return QUEUE.get(chat_id, [])
+
+def pop_queue(chat_id):
+    if chat_id in QUEUE and QUEUE[chat_id]:
+        return QUEUE[chat_id].pop(0)
+
+    return None
+
+def clear_queue(chat_id):
+    if chat_id in QUEUE:
+        QUEUE[chat_id] = []
