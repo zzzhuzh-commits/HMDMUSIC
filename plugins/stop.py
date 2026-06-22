4311dbe4289b622174139f56a12d13099b3042a0
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:42:12 +0200

    Add stop command for stopping the bot

--- plugins/stop.py
+++ plugins/stop.py
@@ -1,1 +1,7 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.text & filters.regex("^ايقاف$"))
+async def stop_command(_, message):
+    await message.reply_text(
+        "⏹ تم إيقاف التشغيل"
+    )
