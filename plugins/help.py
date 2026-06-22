37e120484f9d32170d33d7ab25e9ac183c98b183
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:37:03 +0200

    Implement help command for HMDMUSIC
    Added help command for HMDMUSIC with usage instructions.

--- plugins/help.py
+++ plugins/help.py
@@ -1,1 +1,15 @@
+from pyrogram import filters
+from pyrogram import Client
 
+@Client.on_message(filters.text & filters.regex("^مساعدة$"))
+async def help_message(_, message):
+    await message.reply_text(
+        """
+🎵 أوامر HMDMUSIC
+
+• شغل
+• تخطي
+• ايقاف
+• استئناف
+        """
+    )
