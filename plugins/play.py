28462928ca6a4c0dcb27bd4c912f2227973fd515
zzzhuzh-commits <zzzhuzh@gmail.com>
2026-06-22 08:47:16 +0200

    Update play.py

--- plugins/play.py
+++ plugins/play.py
@@ -1,9 +1,23 @@
 from pyrogram import Client, filters
 
+from HMDMUSIC.core.queue import add_to_queue, get_queue
+
 @Client.on_message(filters.text & filters.regex(r"^شغل (.+)"))
 async def play_command(_, message):
+
     song = message.text[4:].strip()
 
+    add_to_queue(
+        message.chat.id,
+        song
+    )
+
+    position = len(
+        get_queue(message.chat.id)
+    )
+
     await message.reply_text(
-        f"🎵 جاري البحث عن:\n\n{song}"
+        f"🎵 تمت إضافة الأغنية\n\n"
+        f"📌 {song}\n"
+        f"📋 الترتيب: {position}"
     )
